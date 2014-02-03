688d3b745fa6cbb75b413a12e1339a926dce05fa
nabuk <niemyjski.jakub@gmail.com>
2014-02-03 21:17:00 +0100

    ProxyBag class

---
+++ src/ProxySwarm.Domain/ProxyBag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxySwarm.Domain
+{
+    public class ProxyBag
+    {
+        public void Add(params Proxy[] proxy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Proxy Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
