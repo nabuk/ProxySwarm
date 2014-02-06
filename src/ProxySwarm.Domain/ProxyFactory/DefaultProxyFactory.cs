8a3f2b7df2a94bcf89342bc949f7b1e3092c103b
nabuk <niemyjski.jakub@gmail.com>
2014-02-06 18:42:30 +0100

    default proxy factory

---
+++ src/ProxySwarm.Domain/ProxyFactory/DefaultProxyFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ProxySwarm.Domain.ProxyFactory
+{
+    public class DefaultProxyFactory : IProxyFactory
+    {
+        public IList<Proxy> RetrieveProxies(string content)
+        {
+            Func<Match, Proxy> mapToProxyObject = m =>
+            {
+                var parts = m.Value.Split(':');
+                return new Proxy(parts[0], int.Parse(parts[1]));
+            };
+
+            return Regex.Matches(content, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}")
+                .OfType<Match>()
+                .Select(mapToProxyObject)
+                .ToArray();
+        }
+    }
+}
--- src/ProxySwarm.Domain/ProxyFactory/IProxyFactory.cs
+++ src/ProxySwarm.Domain/ProxyFactory/IProxyFactory.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProxySwarm.Domain
+namespace ProxySwarm.Domain.ProxyFactory
 {
     public interface IProxyFactory
     {
