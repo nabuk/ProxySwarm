58ad6dca1efb076e331595bd0b263be6ae4d4d12
Jakub Niemyjski <niemyjski.jakub@gmail.com>
2014-02-07 11:41:02 +0100

    Working Publisher with locking

--- src/ProxySwarm.Domain/Miscellaneous/Publisher.cs
+++ src/ProxySwarm.Domain/Miscellaneous/Publisher.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,28 +11,52 @@ namespace ProxySwarm.Domain.Miscellaneous
     {
         private readonly TimeSpan delay;
         private readonly CancellationToken cancellationToken;
+        private readonly Task cancellationTask;
 
         private T data;
-        private Task publishTask;
 
-        private void PublishWorkerMethod()
+        private bool published = true;
+        private readonly object publishLock = new object();
+
+        private async void PublishMethod()
         {
-            while (!this.cancellationToken.IsCancellationRequested)
+            await Task.WhenAny(Task.Delay(this.delay), this.cancellationTask);
+            this.cancellationToken.ThrowIfCancellationRequested();
+
+            T dataToPublish;
+            lock (this.publishLock)
             {
-                throw new NotImplementedException();
+                this.published = true;
+                dataToPublish = this.data;
             }
+            this.NewDataAvailable(dataToPublish);
         }
 
         internal Publisher(TimeSpan delay, CancellationToken cancellationToken)
         {
             this.delay = delay;
             this.cancellationToken = cancellationToken;
-            this.publishTask = Task.Run((Action)PublishWorkerMethod, cancellationToken);
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(() => tcs.TrySetCanceled(), useSynchronizationContext: false);
+            this.cancellationTask = tcs.Task;
         }
 
         internal void Publish(T data)
         {
-            throw new NotImplementedException();
+            var runNewTask = false;
+
+            lock (this.publishLock)
+            {
+                this.data = data;
+                if (this.published)
+                {
+                    this.published = false;
+                    runNewTask = true;
+                }
+            }
+
+            if (runNewTask)
+                Task.Run((Action)this.PublishMethod);
         }
 
         internal event Action<T> NewDataAvailable = delegate { };
