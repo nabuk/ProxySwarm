4241907bc79e344514ab7c11e5b1a430a997a46d
nabuk <niemyjski.jakub@gmail.com>
2014-02-28 07:51:49 +0100

    Pause works now

--- src/ProxySwarm.Domain/SwarmCoordinator.cs
+++ src/ProxySwarm.Domain/SwarmCoordinator.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
+ 
 namespace ProxySwarm.Domain
 {
     public class SwarmCoordinator : IDisposable
@@ -12,11 +9,12 @@ namespace ProxySwarm.Domain
         private readonly IProxyWorkerFactory proxyWorkerFactory;
         private readonly int maxWorkerCount;
         private readonly ProxyBag proxyBag;
-        
+ 
         private TaskCompletionSource<bool> isRunningCompletionSource = new TaskCompletionSource<bool>();
-        private Task[] tasks;
         private Task isRunningTask;
-
+        private CancellationTokenSource disposeCancellationSource = new CancellationTokenSource();
+        private Task throttlerTask;
+ 
         public SwarmCoordinator(IProxyWorkerFactory proxyWorkerFactory, IObservable<Proxy> proxySource, int maxWorkerCount)
         {
             this.proxyWorkerFactory = proxyWorkerFactory;
@@ -25,64 +23,64 @@ namespace ProxySwarm.Domain
             this.Status = new SwarmCoordinatorStatus { ProxyCounter = this.proxyBag.Counter };
             this.isRunningTask = this.isRunningCompletionSource.Task;
         }
-
+ 
         private async Task WorkerMethod(Proxy proxy)
         {
             this.Status.ConnectionCounter.Increment();
-
+ 
             var success = await this.proxyWorkerFactory.CreateWorkerAsync(proxy);
             if (success)
                 this.Status.SuccessCounter.Increment();
             else
                 this.Status.FailCounter.Increment();
-
+ 
             this.Status.ConnectionCounter.Decrement();
+        }
+ 
+        private async Task ThrottlerMethod(CancellationToken cancellationToken)
+        {
+            using (var throttler = new SemaphoreSlim(initialCount: this.maxWorkerCount, maxCount: this.maxWorkerCount))
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await throttler.WaitAsync();
+                    var proxy = await this.proxyBag.ReceiveAsync(cancellationToken);
+ 
+                    await this.isRunningTask;
+ 
+                    Task.Run(async () =>
+                    {
+                        try { await WorkerMethod(proxy); }
+                        finally { throttler.Release(); }
+                    });
+                }
         }
 
         public void Start()
         {
             if (this.isRunningTask.IsCompleted)
                 return;
-
+ 
             this.isRunningCompletionSource.SetResult(true);
-
-            if (this.tasks == null)
-            {
-                this.tasks = new Task[this.maxWorkerCount];
-
-                Task.Factory.StartNew(async () =>
-                {
-                    using (var throttler = new SemaphoreSlim(initialCount: maxWorkerCount, maxCount: this.maxWorkerCount))
-                        while (true)
-                        {
-                            await throttler.WaitAsync();
-                            var proxy = await this.proxyBag.ReceiveAsync(CancellationToken.None);
-
-                            Task.Run(async () =>
-                            {
-                                try { await WorkerMethod(proxy); }
-                                finally { throttler.Release(); }
-                            });
-                        }
-
-                }, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
-            }
+ 
+            if (this.throttlerTask == null)
+                this.throttlerTask = Task.Factory.StartNew(async () => await this.ThrottlerMethod(this.disposeCancellationSource.Token), TaskCreationOptions.LongRunning);
         }
-
+ 
         public void Pause()
         {
             if (!this.isRunningTask.IsCompleted)
                 return;
-
+ 
             this.isRunningCompletionSource = new TaskCompletionSource<bool>();
             this.isRunningTask = this.isRunningCompletionSource.Task;
         }
-
+ 
         public SwarmCoordinatorStatus Status { get; private set; }
-
+ 
         #region IDisposable
         public void Dispose()
         {
+            this.disposeCancellationSource.Cancel();
             this.proxyBag.Dispose();
         }
         #endregion //IDisposable
