faf84d21ba7b54e8f00b8f410df9a0edd18b4fc6
nabuk <niemyjski.jakub@gmail.com>
2014-02-28 21:21:17 +0100

    WebRequest/WebResponse changed to HttpClient

--- src/ProxySwarm.WpfApp/Concrete/TestProxyWorkerFactory.cs
+++ src/ProxySwarm.WpfApp/Concrete/TestProxyWorkerFactory.cs
@@ -1,10 +1,8 @@
 ﻿using ProxySwarm.Domain;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Cache;
-using System.Text;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,79 +12,46 @@ namespace ProxySwarm.WpfApp.Concrete
     {
         public async Task<bool> CreateWorkerAsync(Proxy proxy)
         {
-            var request = System.Net.WebRequest.CreateHttp("http://www.microsoft.com");
-            request.Method = "GET";
-            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0";
-            request.Timeout = 20000;
-            request.ContinueTimeout = request.Timeout;
-            request.ReadWriteTimeout = request.Timeout;
-            request.ContentLength = 0;
-            request.AllowAutoRedirect = false;
-            request.ProtocolVersion = HttpVersion.Version11;
-            request.KeepAlive = false;
-            request.Proxy = new System.Net.WebProxy(proxy.Address, proxy.Port);
-            request.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
-
-            var cts = new CancellationTokenSource();
-            var cancellationToken = cts.Token;
-            cts.CancelAfter(request.Timeout);
-
-            var cancellationTcs = new TaskCompletionSource<bool>();
-
-            using (cancellationToken.Register(() => cancellationTcs.SetCanceled(), useSynchronizationContext: false))
-            using (var ms = new System.IO.MemoryStream())
+            try
             {
-                HttpWebResponse response = null;
-                try
-                {
-                    var webResponseTask = request.GetResponseAsync();
-                    await Task.WhenAny(webResponseTask, cancellationTcs.Task);
-                    if (cancellationToken.IsCancellationRequested)
+                var httpClient = new HttpClient(
+                    new HttpClientHandler
                     {
-                        request.Abort();
-                        webResponseTask.ContinueWith(responseTask =>
-                        {
-                            if (responseTask.Status == TaskStatus.RanToCompletion)
-                                responseTask.Result.Dispose();
-                        });
-                        
-                        //await Task.WhenAny(webResponseTask);
-                        //if (webResponseTask.Status == TaskStatus.RanToCompletion)
-                        //    webResponseTask.Result.Dispose();
-                        return false;
+                        AllowAutoRedirect = false,
+                        UseCookies = true,
+                        CookieContainer = new CookieContainer(),
+                        Proxy = new WebProxy(proxy.Address, proxy.Port),
+                        UseProxy = true
                     }
+                    , true)
+                {
+                    Timeout = TimeSpan.FromSeconds(20),
+                    MaxResponseContentBufferSize = 256000
+                };
 
-                    using (response = (HttpWebResponse)webResponseTask.Result)
-                    using (var responseStream = response.GetResponseStream())
+                httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0");
+
+                using (httpClient)
+                {
+                    var response = await httpClient.GetAsync("http://www.microsoft.com/en-US/default.aspx", HttpCompletionOption.ResponseContentRead);
+                    using (response)
                     {
-                        if (responseStream == null)
-                            throw new System.Data.NoNullAllowedException("Response stream cannot be null.");
-
-                        var statusCode = (int)response.StatusCode;
-                        if (statusCode >= 300 && statusCode <= 399)
+                        if (!response.IsSuccessStatusCode)
                             return false;
 
-                        var copyTask = responseStream.CopyToAsync(ms, 1024 * 64, cancellationToken);
-                        await Task.WhenAny(copyTask, cancellationTcs.Task);
-                        if (cancellationToken.IsCancellationRequested)
+                        using (var content = response.Content)
                         {
-                            await copyTask;
-                            return false;
+                            //var strContent = await content.ReadAsStringAsync();
+                            //strContent.ToString();
                         }
+                        return true;
                     }
-
-                    ms.Seek(0, System.IO.SeekOrigin.Begin);
-
-                    using (var sr = new System.IO.StreamReader(ms))
-                        await sr.ReadToEndAsync();
-                }
-                catch
-                {
-                    return false;
                 }
             }
-
-            return true;
+            catch
+            {
+                return false;
+            }
         }
     }
 }
