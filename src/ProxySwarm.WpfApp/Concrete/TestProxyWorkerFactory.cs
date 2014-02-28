8fb5759357dc120a62f13e97dfb174e91d6ed6bd
nabuk <niemyjski.jakub@gmail.com>
2014-02-28 07:52:49 +0100

    improved performance

--- src/ProxySwarm.WpfApp/Concrete/TestProxyWorkerFactory.cs
+++ src/ProxySwarm.WpfApp/Concrete/TestProxyWorkerFactory.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,59 +17,67 @@ namespace ProxySwarm.WpfApp.Concrete
             request.Method = "GET";
             request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0";
             request.Timeout = 20000;
+            request.ContinueTimeout = request.Timeout;
+            request.ReadWriteTimeout = request.Timeout;
             request.ContentLength = 0;
+            request.AllowAutoRedirect = false;
+            request.ProtocolVersion = HttpVersion.Version11;
+            request.KeepAlive = false;
             request.Proxy = new System.Net.WebProxy(proxy.Address, proxy.Port);
-            var requestTime = DateTime.UtcNow;
 
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
             cts.CancelAfter(request.Timeout);
 
-            try
-            {
-                var cancellationTcs = new TaskCompletionSource<bool>();
+            var cancellationTcs = new TaskCompletionSource<bool>();
 
-                using (cancellationToken.Register(() => cancellationTcs.SetCanceled(), useSynchronizationContext: false))
-                using (var ms = new System.IO.MemoryStream())
+            using (cancellationToken.Register(() => cancellationTcs.SetCanceled(), useSynchronizationContext: false))
+            using (var ms = new System.IO.MemoryStream())
+            {
+                WebResponse response = null;
+                try
                 {
-                    System.Net.WebResponse response = null;
-                    try
+                    var webResponseTask = request.GetResponseAsync();
+                    await Task.WhenAny(webResponseTask, cancellationTcs.Task);
+                    if (cancellationToken.IsCancellationRequested)
                     {
-                        var webResponseTask = request.GetResponseAsync();
-                        await Task.WhenAny(webResponseTask, cancellationTcs.Task);
-                        response = webResponseTask.Result;
-
-                        using (var responseStream = response.GetResponseStream())
+                        request.Abort();
+                        webResponseTask.ContinueWith(responseTask =>
                         {
-                            if (responseStream == null)
-                                throw new System.Data.NoNullAllowedException("Response stream cannot be null.");
-
-                            var buffer = new byte[1024 * 64];
-                            var readCount = -1;
-                            while (readCount != 0)
-                            {
-                                var readTask = responseStream.ReadAsync(buffer, 0, 1024 * 64, cancellationToken);
-                                await Task.WhenAny(readTask, cancellationTcs.Task);
-                                readCount = readTask.Result;
-                                await ms.WriteAsync(buffer, 0, readCount);
-                            }
-                        }
-
-                        ms.Seek(0, System.IO.SeekOrigin.Begin);
-
-                        using (var sr = new System.IO.StreamReader(ms))
-                            await sr.ReadToEndAsync();
+                            if (responseTask.Status == TaskStatus.RanToCompletion)
+                                responseTask.Result.Dispose();
+                        });
+                        
+                        //await Task.WhenAny(webResponseTask);
+                        //if (webResponseTask.Status == TaskStatus.RanToCompletion)
+                        //    webResponseTask.Result.Dispose();
+                        return false;
                     }
-                    finally
+
+                    using (response = webResponseTask.Result)
+                    using (var responseStream = response.GetResponseStream())
                     {
-                        if (response != null)
-                            response.Dispose();
+                        if (responseStream == null)
+                            throw new System.Data.NoNullAllowedException("Response stream cannot be null.");
+
+                        var copyTask = responseStream.CopyToAsync(ms, 1024 * 64, cancellationToken);
+                        await Task.WhenAny(copyTask, cancellationTcs.Task);
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            await copyTask;
+                            return false;
+                        }
                     }
+
+                    ms.Seek(0, System.IO.SeekOrigin.Begin);
+
+                    using (var sr = new System.IO.StreamReader(ms))
+                        await sr.ReadToEndAsync();
                 }
-            }
-            catch
-            {
-                return false;
+                catch
+                {
+                    return false;
+                }
             }
 
             return true;
