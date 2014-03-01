e525c2351ce16915b9d2e37b2e5d4a8414eb0412
nabuk <niemyjski.jakub@gmail.com>
2014-03-01 14:48:18 +0100

    name refactor

--- src/ProxySwarm.Domain/Isolation/Isolated.cs
+++ src/ProxySwarm.Domain/Isolation/Isolated.cs
@@ -6,34 +6,31 @@ namespace ProxySwarm.Domain.Isolation
 
     public sealed class Isolated<T> : IDisposable where T : MarshalByRefObject
     {
-        private AppDomain _domain;
-        private T _value;
+        private AppDomain domain;
+        private T value;
 
         public Isolated()
         {
-            _domain = AppDomain.CreateDomain("Isolated:" + Guid.NewGuid(),
-               null, AppDomain.CurrentDomain.SetupInformation);
+            this.domain = AppDomain.CreateDomain("Isolated:" + Guid.NewGuid(), null, AppDomain.CurrentDomain.SetupInformation);
 
-            Type type = typeof(T);
-
-            _value = (T)_domain.CreateInstanceAndUnwrap(type.Assembly.FullName, type.FullName);
+            var type = typeof(T);
+            this.value = (T)this.domain.CreateInstanceAndUnwrap(type.Assembly.FullName, type.FullName);
         }
 
         public T Value
         {
             get
             {
-                return _value;
+                return this.value;
             }
         }
 
         public void Dispose()
         {
-            if (_domain != null)
+            if (this.domain != null)
             {
-                AppDomain.Unload(_domain);
-
-                _domain = null;
+                AppDomain.Unload(this.domain);
+                this.domain = null;
             }
         }
     }
